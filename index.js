384211bb5f8de2c6608ecc0962fbd15679ed5b65
chmonster <perry.moore@gmail.com>
2022-08-27 12:52:38 -0230

    almost there

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 $(document).ready(function () {
 
-  $('#canoe').load(function() {
-    const mrcWidth=Math.floor(50+50*Math.random()).toString()+"px";
-    const imgWidth=$(this).width();
-    const imgHeight=$(this).height();
+  // $('#canoe').load(function() {
+  //   const mrcWidth=Math.floor(50+50*Math.random()).toString()+"px";
+  //   const imgWidth=$(this).width();
+  //   const imgHeight=$(this).height();
 
-    $('#mrcanoehead>img').attr({
-      "width": mrcWidth,
-      "height": "auto"
+  //   $('#mrcanoehead>img').attr({
+  //     "width": mrcWidth,
+  //     "height": "auto"
+  //   });
+  //   $('#mrcanoehead>img').css({
+  //     "left": Math.floor(imgWidth*Math.random()).toString()+"px",
+  //     "top": Math.floor(imgHeight*Math.random()).toString()+"px"
+  //   });
+  // });
+
+
+  const loren = function(label) {
+    $.ajax({
+      type: 'GET',
+      url: 'http://metaphorpsum.com/paragraphs/3?p=true', 
+      crossDomain: true,
+      dataType: 'html',
+      success: function(result) { 
+        $(label).html(result);
+      },
+      error: function(error) {
+        $('article').html(error.status + ' ' + error.statusText);
+      }
     });
-    $('#mrcanoehead>img').css({
-      "left": Math.floor(imgWidth*Math.random()).toString()+"px",
-      "top": Math.floor(imgHeight*Math.random()).toString()+"px"
-    });
-  });
+  };
+
+  loren('#loren1');
+  loren('#loren2');
 
   $('nav').hide();
   $('#hamburger').click(function() {
-     $('nav').toggle('fast', 'swing');
+    //  $('nav').toggle({ direction: "left" },'fast', 'swing');
+    $('nav').animate({width: 'toggle'}, 'fast', 'swing');
   });
+
+
 
 });
 
-// if ( $('nav').css('visibility') == 'hidden' ){
-//   $('nav').css('visibility','visible');
-// } else {
-//   $('nav').css('visibility','hidden');
-// }

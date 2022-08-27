723f26b30ae312a7e09a03bd65424aa67398f2bf
chmonster <perry.moore@gmail.com>
2022-08-27 10:51:12 -0230

    header in progress

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
-  $('#mrcanoehead').css({"top":"20%", "left":"40%"});
-})
+
+  $('#canoe').load(function() {
+    const mrcWidth=Math.floor(50+50*Math.random()).toString()+"px";
+    const imgWidth=$(this).width();
+    const imgHeight=$(this).height();
+
+    $('#mrcanoehead>img').attr({
+      "width": mrcWidth,
+      "height": "auto"
+    });
+    $('#mrcanoehead>img').css({
+      "left": Math.floor(imgWidth*Math.random()).toString()+"px",
+      "top": Math.floor(imgHeight*Math.random()).toString()+"px"
+    });
+  });
+
+  $('nav').hide();
+  $('#hamburger').click(function() {
+     $('nav').toggle('fast', 'swing');
+  });
+
+});
+
+// if ( $('nav').css('visibility') == 'hidden' ){
+//   $('nav').css('visibility','visible');
+// } else {
+//   $('nav').css('visibility','hidden');
+// }

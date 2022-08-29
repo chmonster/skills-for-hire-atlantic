6e5bc538e09a9eda02f285063aebb0bb1e717648
chmonster <perry.moore@gmail.com>
2022-08-29 13:16:34 -0230

    more MrC fiddling

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
     const mrcWidth=$('#mrcanoehead-img').width();
     const mrcHeight=$('#mrcanoehead-img').height();
     const bubbleWidth=$('#bubble').width();
-    const bubbleHeight=-$('#bubble').height();
+    const bubbleHeight=$('#bubble').height();
 
     const allowedWidth=imgWidth-bubbleWidth;
     const allowedHeight=imgHeight-mrcHeight-1.5*bubbleHeight;
@@ -55,19 +55,15 @@ $(document).ready(function () {
     const mrcLeft=(Math.floor(allowedWidth*Math.random()));
     const mrcTop=(Math.floor(1.5*bubbleHeight+allowedHeight*Math.random()));
 
-    //console.log(mrcWidth, mrcHeight);
-
+    console.log(allowedWidth, allowedHeight, mrcLeft, mrcTop);
     
     $('#mrcanoehead').css({
       "left": (mrcLeft).toString()+'px',
       "top": (mrcTop).toString()+'px'
     });
-    // $('#bubble::before').css({
-
-    // });
     $('#bubble').css({
-      'left': (mrcWidth-0.4*bubbleWidth).toString() +'px',
-      'top' : (-0.1*mrcHeight+bubbleHeight).toString() +'px'
+      'left': Math.floor(mrcWidth-0.3*bubbleWidth).toString() +'px',
+      'top' : Math.floor(-0.25*mrcHeight-bubbleHeight).toString() +'px'
     });
 
     $('#mrcanoehead').show();
@@ -93,7 +89,11 @@ $(document).ready(function () {
     positionMrC();
   });
 
-  $('#mrcanoehead').hover(function() {
+  $('#mrcanoehead-img').hover(function() {
+    positionMrC();
+  });
+
+  $('bubble').hover(function() {
     positionMrC();
   });
 

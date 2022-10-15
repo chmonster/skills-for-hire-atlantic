f18e47814cc47b482acf0eb64621a1b0de1d0086
chmonster <perry.moore@gmail.com>
2022-10-15 09:55:16 -0230

    fixed speech bubble rendering

--- index.js
+++ index.js
@@ -1,94 +1,95 @@
+const pixelString = value => Math.floor(value).toString().concat('px')
+
+const positionMrC = () => {
+  const imgWidth = $('#photo-area').width()
+  const imgHeight = $('#photo-area').height()
+
+  // % of image width
+  const minImgWidth = 0.05
+  const maxImgWidth = 0.1
+  // from *.png
+  const aspectRatio = 607 / 400
+
+  const mrcWidth =
+    imgWidth * (minImgWidth + Math.random() * (maxImgWidth - minImgWidth))
+
+  const mrcHeight = mrcWidth * aspectRatio
+
+  $('#mrcanoehead-img').attr({
+    // width: pixelString(initialImgWidth),
+    // height: 'auto',
+    width: mrcWidth,
+    height: mrcHeight,
+  })
+
+  //get resultant sizes
+  // const mrcWidth = $('#mrcanoehead-img').width()
+  // const mrcHeight = $('#mrcanoehead-img').height()
+  const bubbleWidth = mrcHeight
+  const bubbleHeight = mrcWidth
+  const bubbleFontSize = bubbleWidth / 10
+  const borderSize = bubbleFontSize / 4
+
+  //boundary for placement
+  const minLeft = 0
+  const minRight = imgWidth - bubbleWidth - mrcWidth
+  const minTop = bubbleHeight
+  const minBottom = imgHeight - mrcHeight
+
+  const mrcLeft = minLeft + (minRight - minLeft) * Math.random()
+  const mrcTop = minTop + (minBottom - minTop) * Math.random()
+  //relative to container #mrcanoehead !
+  const bubbleLeft = mrcWidth
+  const bubbleTop = -bubbleHeight
+
+  const tailWidth = mrcWidth + 0.5 * bubbleWidth
+  const tailHeight = bubbleHeight
+  const tailLeft = 0
+  const tailTop = -0.5 * bubbleHeight
+
+  //execute positioning
+  $('#mrcanoehead').css({
+    left: pixelString(mrcLeft),
+    top: pixelString(mrcTop),
+  })
+  $('#bubble').css({
+    left: pixelString(bubbleLeft),
+    top: pixelString(bubbleTop),
+    width: pixelString(bubbleWidth),
+    height: pixelString(bubbleHeight),
+    fontSize: pixelString(bubbleFontSize),
+    border: pixelString(borderSize).concat(' solid black'),
+    boxShadow:
+      '0 ' + pixelString(borderSize / 2) + ' 0 ' + pixelString(borderSize / 4),
+  })
+  $('#bubble-tail').css({
+    left: pixelString(tailLeft - borderSize),
+    top: pixelString(tailTop - borderSize),
+    width: pixelString(tailWidth),
+    height: pixelString(tailHeight),
+    // border: (bubbleFontSize / 4).toString() + 'px solid black',
+    boxShadow:
+      pixelString(1 * borderSize) +
+      ' ' +
+      pixelString(0.5 * borderSize) +
+      ' ' +
+      '#000, ' +
+      pixelString(3 * borderSize) +
+      ' ' +
+      pixelString(0.5 * borderSize) +
+      ' ' +
+      '#ffd, ' +
+      pixelString(4 * borderSize) +
+      ' ' +
+      pixelString(1 * borderSize) +
+      ' ' +
+      ' #000',
+  })
+}
+
 $(document).ready(function () {
-  const pixelString = value => Math.floor(value).toString().concat('px')
-
-  const positionMrC = function () {
-    const imgWidth = $('#photo-area').width()
-    const imgHeight = $('#photo-area').height()
-
-    // % of image width
-    const minImgWidth = 0.05
-    const maxImgWidth = 0.1
-
-    const initialImgWidth =
-      imgWidth * (minImgWidth + Math.random() * (maxImgWidth - minImgWidth))
-
-    $('#mrcanoehead-img').attr({
-      width: pixelString(initialImgWidth),
-      height: 'auto',
-    })
-
-    //get resultant sizes
-    const mrcWidth = $('#mrcanoehead-img').width()
-    const mrcHeight = $('#mrcanoehead-img').height()
-    const bubbleWidth = mrcHeight
-    const bubbleHeight = mrcWidth
-    const bubbleFontSize = bubbleWidth / 10
-    const borderSize = bubbleFontSize / 4
-
-    //boundary for placement
-    const minLeft = 0
-    const minRight = imgWidth - bubbleWidth - mrcWidth
-    const minTop = bubbleHeight
-    const minBottom = imgHeight - mrcHeight
-
-    const mrcLeft = minLeft + (minRight - minLeft) * Math.random()
-    const mrcTop = minTop + (minBottom - minTop) * Math.random()
-    //relative to container #mrcanoehead !
-    const bubbleLeft = mrcWidth
-    const bubbleTop = -bubbleHeight
-
-    const tailWidth = mrcWidth + 0.5 * bubbleWidth
-    const tailHeight = bubbleHeight
-    const tailLeft = 0
-    const tailTop = -0.5 * bubbleHeight
-
-    //execute positioning
-    $('#mrcanoehead').css({
-      left: pixelString(mrcLeft),
-      top: pixelString(mrcTop),
-    })
-    $('#bubble').css({
-      left: pixelString(bubbleLeft),
-      top: pixelString(bubbleTop),
-      width: pixelString(bubbleWidth),
-      height: pixelString(bubbleHeight),
-      fontSize: pixelString(bubbleFontSize),
-      border: pixelString(borderSize).concat(' solid black'),
-      boxShadow:
-        '0 ' +
-        pixelString(borderSize / 2) +
-        ' 0 ' +
-        pixelString(borderSize / 4),
-    })
-    $('#bubble-tail').css({
-      left: pixelString(tailLeft - borderSize),
-      top: pixelString(tailTop - borderSize),
-      width: pixelString(tailWidth),
-      height: pixelString(tailHeight),
-      // border: (bubbleFontSize / 4).toString() + 'px solid black',
-      boxShadow:
-        pixelString(1 * borderSize) +
-        ' ' +
-        pixelString(0.5 * borderSize) +
-        ' ' +
-        '#000, ' +
-        pixelString(3 * borderSize) +
-        ' ' +
-        pixelString(0.5 * borderSize) +
-        ' ' +
-        '#ffd, ' +
-        pixelString(4 * borderSize) +
-        ' ' +
-        pixelString(1 * borderSize) +
-        ' ' +
-        ' #000',
-    })
-
-    // $('#mrcanoehead').show()
-  }
-
-  // $('#mrcanoehead').hide()
   $('nav').hide()
+
   $('#hamburger').click(function () {
     if ($(window).width() > 400) {
       $('nav').animate({ width: 'toggle' }, 'fast', 'swing')
@@ -102,11 +103,7 @@ $(document).ready(function () {
     $('#mrcanoehead').show()
   })
 
-  $('#mrcanoehead-img').hover(function () {
-    positionMrC()
-  })
-
-  $('#bubble').hover(function () {
+  $('#mrcanoehead').hover(function () {
     positionMrC()
   })
 
